fbed049444fad0bd71357307f4438ec6b35492ef
ericflorentin <florentin.eric@gmail.com>
2024-06-08 22:05:39 +0100

    update with clipboard functionality

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 function clipboardCopy(id) {
-  var copyText = document.getElementById(id);
+  let copyText = document.getElementById(id);
   navigator.clipboard.writeText(copyText.innerText);
+}
+
+function showTooltip(id) {
+  let copyBox = document.getElementById(id);
+  let tooltip = document.createElement("p");
+  tooltip.classList.add("tooltip");
+  tooltip.textContent = "Copied!";
+  copyBox.appendChild(tooltip);
+
+  setTimeout(() => {
+    copyBox.removeChild(tooltip);
+  }, 1500);
 }
